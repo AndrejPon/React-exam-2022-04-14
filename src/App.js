ab12e333c5248b9f2ea3d38fdc1f37a659de2a63
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-04-13 18:23:35 +0300

    Pages, routes created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
+import AddPage from './pages/AddPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <Switch>
+        <Route path={'/home'} exact>
+          <HomePage />
+        </Route>
+        <Route path={'/register'}>
+          <RegisterPage />
+        </Route>
+        <Route path={'/login'}>
+          <LoginPage />
+        </Route>
+        <Route path={'/add'}>
+          <AddPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
