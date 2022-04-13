411723341e253969afa27f322d5c3e52fe28719d
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-04-13 18:50:33 +0300

    Nav links + css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
 import AddPage from './pages/AddPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -9,6 +10,7 @@ import RegisterPage from './pages/RegisterPage';
 function App() {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route path={'/home'} exact>
           <HomePage />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+import css from './Header.module.css';
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <img
+        className={css.img}
+        src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png'
+        alt='Google logo'
+      />
+      <nav className={css.navLinks}>
+        <Link to='/home'>Home</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/login'>Login</Link>
+        <Link to='/add'>Add</Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
